48275e58b3e14e92337f8ef6d3a92932c1743691
myusername <myemail>
2020-12-28 16:22:00 +0200

    button

--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import Button from "./Button";
 
 
 export default function Main() {
   return (
     <Router>
+
       <Switch>
         <Route path="/">
-        <Login />
+          <Login />
         </Route>
       </Switch>
     </Router>
