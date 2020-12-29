e5c591bf34c65b0548d9a933de7d030a4c8f9f92
myusername <myemail>
2020-12-29 19:40:25 +0200

    I started building a first screen

--- packages/client/src/components/HugsLogo.tsx
+++ packages/client/src/components/HugsLogo.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { COLORS } from "./Colors.js";
 import { makeStyles } from "@material-ui/core/styles";
 import HugsIcon from "../icons/HugsIcon.svg";
 import { ReactSVG } from "react-svg";
 
 interface Props {
-  width: string;
-  height:string
+  width : string;
+  height : string;
 }
 
+
 const HugsLogo = (props:Props) => {
+  const useStyles = makeStyles({
+    logo: {
+      
+        width: props.width,
+        height: props.height,    
+    },
+   
+  });
+  const classes = useStyles();
+
   return (
     <ReactSVG
       beforeInjection={(svg) => {
-        svg.setAttribute('style', `width: ${props.width}`)
-        svg.setAttribute('style', `height: ${props.height}`)
+        svg.classList.add(classes.logo)
     }}
       src={HugsIcon}
     />
--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import FirstScreen from "./FirstScreen";
+import Login from "./Login";
 
 
 
 export default function Main() {
   return (
     <Router>
-
       <Switch>
-        <Route path="/">
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/first">
           <FirstScreen />
         </Route>
       </Switch>
