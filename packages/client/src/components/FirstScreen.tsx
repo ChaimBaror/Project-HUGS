1f8cf8ea79b1612695bc305c692693ef612c9126
myusername <myemail>
2020-12-30 14:40:11 +0200

    First Screen

--- packages/client/src/components/FirstScreen.tsx
+++ packages/client/src/components/FirstScreen.tsx
@@ -8,46 +8,45 @@ import BackArrow from './BackArrow';
 import './Screens.css';
 
 
-const FirstScreen = () => {
+export class FirstScreen extends Component {
+    render() {
 
-    return (
-        <div className='root'>
-            <div className='screenTop'>
-                <HugsLogo width="15%" height="auth" />
-                <div className='backArrow'>
-                    <BackArrow />
+        return (
+            <div className='root'>
+                <div className='screenTop'>
+                    <HugsLogo width="15%" height="auth" />
+                    <div className='backArrow'>
+                        <BackArrow />
+                    </div>
                 </div>
-            </div>
 
 
-            <div className='screen'>
-                <div className='divText'>
-                    <Text />עבור מי נפתח את מעגל המשפחה?
+                <div className='screen'>
+                    <div className='divText'>
+                        <Text />עבור מי נפתח את מעגל המשפחה?
                 </div>
-                <div className='divRadioButton'>
+
                     <div className="divRadioButton">
-
                         <RadioButton />
                     </div>
 
-                </div>
-                <div className="textImage">
-                    <div className="divText"> העלו תמונה</div><div className="center">
-                        <button className="imagePicker">+</button>
+                    <div className="textImage">
+                        <div className="divText"> העלו תמונה</div><div className="center">
+                            <button className="imagePicker">+</button>
+                            {/* <ImagePicker /> */}
+                        </div>
                     </div>
+                    <Button
+                        text="הבא"
+                        width="65%"
+                        height="6.3%"
+                        radius="10px"
+                        background="rgba(247, 105, 64, 1)"
+                        color="white" />
                 </div>
-                {/* <ImagePicker /> */}
-                <Button
-                    text="הבא"
-                    width="65%"
-                    height="6.3%"
-                    radius="10px"
-                    background="rgba(247, 105, 64, 1)"
-                    color="white"
-                />
             </div>
-        </div>
-    )
+        )
+    }
 
 }
 
