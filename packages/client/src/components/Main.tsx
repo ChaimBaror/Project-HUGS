6e3587733a3cf77dcdabe79f27630f95080895d2
leah1berkovits <leah.inmed@gmail.com>
2021-01-03 10:28:06 +0200

    test example

--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
-
+import Test from '../test/Test'
 
 export default function Main() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+       
+
+        <Route path="/test">
+        <Test />
+        </Route>
+
+         <Route path="/">
         <Login />
         </Route>
       </Switch>
