ca4b1b9b44ba3c9b062c71f2ad63f2d6f9cfdc04
myusername <myemail>
2021-01-07 17:38:43 +0200

    Container

--- packages/client/src/components/Container.tsx
+++ packages/client/src/components/Container.tsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { COLORS } from "./Colors.js";
 import { makeStyles } from '@material-ui/core/styles';
+import BackArrow from "./BackArrow";
+import HugsLogo from './HugsLogo';
+import RadioButton from './RadioButton';
+import Button from './Button';
 
 const useStyles = makeStyles({
   root: {
     background: COLORS.main_background,
-    height:'100%',
-    width:'100%',
-    position:'absolute'
+    height: '100%',
+    width: '100%',
+    position: 'absolute'
   },
 });
 
-const Container = () => {
+const Container = (props: any) => {
   const classes = useStyles();
 
-  return(
+  return (
     <div className={classes.root}>
-        <></>
+
+      <div className='root'>
+        <header className='screenTop logo'>
+
+          <div className='screenTop logo'>
+            <div className='backArrow'>
+              {props.BackArrow}
+            </div>
+            {props.HugsLogo}
+          </div>
+        </header>
+
+
+        <div className='screen'>
+       
+          {props.Button}
+        </div>
+      </div>
     </div>
   )
 };
--- packages/client/src/components/Main.tsx
+++ packages/client/src/components/Main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import FirstScreen from "./FirstScreen";
+import UserInput from "./InputUser/InputUser";
 import Login from "./Login";
+import Container from './Container';
+import BackArrow from "./BackArrow";
+import HugsLogo from "./HugsLogo";
+import Button from './Button';
 
 
 
@@ -10,7 +15,22 @@ export default function Main() {
     <Router>
       <Switch>
         <Route path="/" exact > <Login /></Route>
-        <Route path="/first"> <FirstScreen /> </Route>
+      
+      
+        <Route path="/Container"> <Container
+         BackArrow={<BackArrow />}
+         HugsLogo ={ <HugsLogo width="60%" height="60%" />}
+          Button={<Button
+          text="הבא"
+          width="65%"
+          height="6.3%"
+          radius="10px"
+          background="rgba(247, 105, 64, 1)"
+          color="white" />}
+        /> </Route>
+
+
+       
       </Switch>
     </Router>
   );
